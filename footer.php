ec81f3aa62cddc4b49a4e0bdfb72e85e98e90c9c
Ramon Geronimo <rgero215@gmail.com>
2018-10-19 16:54:37 -0700

    frontend files

---
+++ footer.php
@@ -0,0 +1,12 @@
+<footer>
+	<div class="row">
+		<div class="col-lg-12">
+			<ul class="list-unstyled">
+				<li><a href="<?php echo base_url();?>index.php?general/faq">Faq</a></li>
+				<li><a href="<?php echo base_url();?>index.php?general/privacypolicy">Privacy Policy</a></li>
+				<li><a href="<?php echo base_url();?>index.php?general/refundpolicy">Refund Policy</a></li>
+			</ul>
+			<p>Made by <a href="https://github.com/RGero215" rel="nofollow">RGero215</a>. Contact me at <a href="mailto:rgero215@gmail.com">rgero215@gmail.com</a>.</p>
+		</div>
+	</div>
+</footer>
---
+++ landing (1).php
@@ -0,0 +1,351 @@
+<!-- TOP LANDING SECTION -->
+<div style="height:93vh;width:100%;background-image: url(<?php echo base_url().'assets/frontend/flixer/images/home_top_banner.jpg';?>)">
+	<!-- logo -->
+	<div style="float: left;">
+		<a href="<?php echo base_url();?>index.php?home">
+		<img src="<?php echo base_url();?>/assets/global/logo.png" style="margin: 18px 18px; height: 45px;" />
+		</a>
+	</div>
+	<div style="float: right;margin: 18px 18px; height: 50px;" class="hidden-xs">
+		<a href="<?php echo base_url();?>index.php?home/signin" class="btn btn-danger">Sign in</a>
+	</div>
+	
+	<!-- promo text visible for large devices -->
+	<div style="font-size: 40px;font-weight: bold;clear: both;padding: 200px 0px 0px 50px;color: #fff;" class="hidden-xs">
+		What Facebook, Microsoft, Snapchat, Yahoo and Google have in common? They all started in a dorm room.
+		<div style="font-size: 20px; letter-spacing: .2px; color: #fff; font-weight: 400;">
+			Join the new generation of founders as they go from the idea to launch and be one of the first to invest in emerging tech when is affordable.
+		</div>
+		<a href="<?php echo base_url();?>index.php?home/signup" class="btn btn-danger btn-lg" >JOIN TODAY</a>
+		<a href="http://rising2dtop.org/" class="btn btn-danger btn-lg" >CAMPAIGNS</a>
+	</div>
+	
+	<!-- promo text visible for small devices -->
+	<div style="font-size: 30px;font-weight: bold;clear: both;padding: 80px 0px 0px 10px;color: #fff;" class="hidden-lg hidden-sm hidden-md">
+		What Facebook, Microsoft, Snapchat, Yahoo and Google have in common? They all started in a dorm room.
+		<div style="font-size: 15px; letter-spacing: .2px; color: #fff; font-weight: 400;">
+			Join the new generation of founders as they go from the idea to launch and be one of the first to invest in emerging tech when is affordable.
+		</div>
+                <div class="row" style="text-align: center; margin-top: 12px;">
+                    <div class="col" style="display: inline-block;">
+                        <a href="<?php echo base_url();?>index.php?home/signup" class="btn btn-danger btn-lg" >JOIN TODAY</a>
+		    </div>
+                    <div class="col" style="display: inline-block;">
+		        <a href="<?php echo base_url();?>index.php?home/signin" class="btn btn-danger btn-lg" >SIGN IN</a>
+                    </div>
+                    <div class="col" style="display: inline-block;">
+		        <a href="http://rising2dtop.org/" class="btn btn-danger btn-lg" >CAMPAIGNS</a>
+                    
+                </div>
+                
+                    </div>
+		
+	</div>
+</div>
+<!-- DESCRIPTION LANDING SECTION -->
+<!-- promo text visible for large devices -->
+<div style="text-align: center; font-size: 85px;font-weight: bold; color: #FF0000" class="hidden-xs">
+	What is DormStartups.tv?
+</div>
+
+<!-- promo text visible for small devices -->
+<div style=" margin-top: 20px; text-align: center; font-size: 45px;font-weight: bold; color: #FF0000" class="hidden-lg hidden-sm hidden-md">
+	What is DormStartups.tv?
+</div>
+<div style="height:93vh;width:100%;background-image: url(<?php echo base_url().'assets/frontend/flixer/images/idea.jpg';?>)">
+	
+	<!-- promo text visible for large devices -->
+	<div style="font-size: 20px; letter-spacing: .2px; color: #fff; font-weight: 400; margin: 20px;" class="hidden-xs">
+			
+	<div style="margin: 20px;">
+	DormStartups.tv  is a subscription video on demand (SVOD) platform that showcases the stories of tech students working on their projects and emerging technologies. Users can find a cofounder with technical skills or pre-seed projects following two unique investment format we like to call:</br>
+&nbsp &nbsp A) Base Income Salary Agreement </br>
+&nbsp &nbsp B) Pre Initial Public Offering
+		<h3 style="color: #FF0000">What is Base Income Salary Agreement?</h3>
+
+Base Salary Investment Agreement is when a person willing to invest their savings, agrees to pay a base salary to a developer for x amount of years in exchange of a small percentage of the company&#39s equity.</br>
+Lets assume that an average Joe has a Savings of 120k and is looking for an investment in a tech company. Following the Base Salary Investment Agreement approach the average Joe can agree to pay $500 weekly for 24 months or $2,000 a month for 2 years in exchange of 5% equity instead of taking a big risk and spending 48K from his/her savings as an initial investment.
+This approach gives any average Joe the opportunity to invest in any emerging technology company and also gives the developer the flexibility to work on their own project idea. 
+A person can invest in multiple projects making agreements that work best for them like for example instead of $500 weekly for 2 years in exchange of 5% in one company, a multiple investment of $250 weekly for 2 years in exchange of 2.5% in two 2 different companies. 
+
+<h3 style="color: #FF0000">What is Pre Initial Public Offering?</h3> Lets assume that an average Joe can’t afford to pay for a Base Salary Investment Agreement and want to buy instead independent shares.  Using Pre Initial Public Offering an average Joe can buy shares for $100 each.  
+Only 1,000 shares will be available for sale or the equivalent to 10%.
+<h3 style="color: #FF0000">What is a SAFE?</h3>
+SAFE (Simple Agreement for Future Equity) where investor receives the right to purchase stock in a future equity round (when one occurs) subject to certain parameters set in advance in the SAFE.
+	</div>
+	
+
+	</div>
+	
+	<!-- promo text visible for small devices -->
+	<div style="font-size: 15px; letter-spacing: .2px; color: #fff; font-weight: 400; " class="hidden-lg hidden-sm hidden-md">
+		<div style="margin: 20px;">
+	DormStartups.tv  is a subscription video on demand (SVOD) platform that showcases the stories of tech students working on their projects and emerging technologies. Users can find a cofounder with technical skills or pre-seed projects following two unique investment format we like to call:</br>
+&nbsp &nbsp A) Base Income Salary Agreement </br>
+&nbsp &nbsp B) Pre Initial Public Offering
+		<h3 style="color: #FF0000">What is Base Income Salary Agreement?</h3>
+
+Base Salary Investment Agreement is when a person willing to invest their savings, agrees to pay a base salary to a developer for x amount of years in exchange of a small percentage of the company&#39s equity.</br>
+Lets assume that an average Joe has a Savings of 120k and is looking for an investment in a tech company. Following the Base Salary Investment Agreement approach the average Joe can agree to pay $500 weekly for 24 months or $2,000 a month for 2 years in exchange of 5% equity instead of taking a big risk and spending 48K from his/her savings as an initial investment.
+This approach gives any average Joe the opportunity to invest in any emerging technology company and also gives the developer the flexibility to work on their own project idea. 
+A person can invest in multiple projects making agreements that work best for them like for example instead of $500 weekly for 2 years in exchange of 5% in one company, a multiple investment of $250 weekly for 2 years in exchange of 2.5% in two 2 different companies. 
+
+<h3 style="color: #FF0000">What is Pre Initial Public Offering?</h3> Lets assume that an average Joe can’t afford to pay for a Base Salary Investment Agreement and want to buy instead independent shares.  Using Pre Initial Public Offering an average Joe can buy shares for $100 each.  
+Only 1,000 shares will be available for sale or the equivalent to 10%.
+<h3 style="color: #FF0000">What is a SAFE?</h3>
+SAFE (Simple Agreement for Future Equity) where investor receives the right to purchase stock in a future equity round (when one occurs) subject to certain parameters set in advance in the SAFE.
+<h3 style="color: #FF0000">Some Benefits?</h3>
+<div>
+Benefits for Tech Students:</br>
+&#9655 Network of Investors</br>
+&#9655 Over 100 mentors including the founders of AngelList, Uber, Make School, LendUp, codeSpark.</br>
+&#9655 $10,000 check in a SAFE:  The lack of a Term (maturity date) gives lots of flexibility. The lack of an Interest Rate saves Startups a little equity dilution in the future when the SAFE converts. </br>
+&#9655 No interest rate since a SAFE is not a debt instrument </br> 
+&#9655 One-document security without numerous terms to negotiate </br>
+&#9655 Reality TV Show Episodes about their work and background </br>
+&#9655 Ecosystem to showcase their project for potential Base Salary Income Agreement </br> 
+&#9655 Cofounder offers for emerging technology ideas </br>
+&#9655 Private event with industry leaders
+		
+</div>
+<hr>
+<div>
+Benefits for Average Joe:</br>
+&#9655 Companies are hand picked and go through a complex evaluation process before being selected and after being selected the founders work with mentors and industry experts to achieve their benchmarks</br>
+&#9655 Companies in our portfolio are power by Kapor Venture Capital who main goal is to help the founders succeed by mentoring and helping in the funding rounds process connecting the startups with potential investors</br>
+&#9655 No need to be part of a selected investors club or have any investor credentials to invest</br>
+&#9655 Does not need to earned $200,000 or more in income over the past two years to qualify as an accredited investor</br> 
+&#9655 No need to wait for a company to go public to buy shares</br>
+&#9655 Affordable price and access to information to early stage startups</br>
+&#9655 Be one of the first to invest on emerging technologies</br> 
+&#9655 Early exit option to sell back your share to the company after future rounds of funding</br>
+		
+</div>
+
+
+
+
+	</div> 
+
+	</div>
+
+</div>
+<!-- STARTUPS LANDING SECTION -->
+<!-- promo text visible for large devices -->
+<div style="text-align: center; font-size: 85px;font-weight: bold; color: #FF0000" class="hidden-xs">
+	Some of the benefits
+</div>
+
+<div style="height:93vh;width:100%;background-image: url(<?php echo base_url().'assets/frontend/flixer/images/startups.jpg';?>)">
+	
+	
+	<!-- promo text visible for large devices -->
+	
+	<div class="row hidden-xs">
+    		<div class="col-sm-6" style="border-right: solid white 2px">
+                        <div style="margin: -10px 20px -7px 20px">
+                            <h3 style="text-align: center;"> Benefits for Tech Students </h3>
+                             <div></br>
+                                 <h5>&#9655 Network of Investors</h5></br>
+                                 <h5>&#9655 Over 100 mentors including the founders of AngelList, Uber, Make School, LendUp, codeSpark</h5></br>
+                                 <h5>&#9655 $10,000 check in a SAFE:  The lack of a Term (maturity date) gives lots of flexibility. The lack of an Interest Rate saves Startups a little equity dilution in the future when the SAFE converts</h5></br>
+                                 <h5>&#9655 No interest rate since a SAFE is not a debt instrument</h5></br>
+                                 <h5>&#9655 One-document security without numerous terms to negotiate </h5></br>
+                                 <h5>&#9655 Reality TV Show Episodes about their work and background</h5></br>
+                                 <h5>&#9655 Ecosystem to showcase their project for potential Base Salary Income Agreement or Pre Initial Public Offering</h5></br>
+                                 <h5>&#9655 Cofounder offers for emerging technology ideas</h5></br>
+                                 <h5>&#9655 Private event with industry leaders </h5></br>
+                             </div>
+                        </div>
+      			
+    		</div>
+
+    		<div class="col-sm-6">
+                        <div style="margin: -10px 20px -7px 20px">
+                            <h3 style="text-align: center;"> Benefits for Average Joe </h3>
+                             <div></br>
+                                 <h5>&#9655 Companies are hand picked and go through a complex evaluation process before being selected and after being selected the founders work with mentors and industry experts to achieve their benchmarks
+</h5></br>
+                                 <h5>&#9655 Companies in our portfolio are power by Investment Banks and Venture Capital who main goal is to help the founders succeed by mentoring and helping in the funding rounds process connecting the startups with potential investors </h5></br>
+                                 <h5>&#9655 No need to be part of a selected investors club or have any investor credentials to invest
+</h5></br>
+                                 <h5>&#9655 Does not need to earned $200,000 or more in income over the past two years to qualify as an accredited investor </h5></br>
+                                 <h5>&#9655 No need to wait for a company to go public to buy shares </h5></br>
+                                 <h5>&#9655 Affordable price and access to information to early stage startups </h5></br>
+                                 <h5>&#9655 Be one of the first to invest on emerging technologies </h5></br>
+                                 <h5>&#9655 Early exit option to sell back your share to the company after future rounds of funding </h5></br>
+                             </div>
+                        </div>
+      			
+    		</div>
+  	</div>
+  
+</div>
+<!-- promo text visible for small devices -->
+	<div style="height: 800px;" class="hidden-lg hidden-sm hidden-md">
+		
+		
+	</div>
+<!-- LAST TAB SECTION -->
+<div class="container">
+	<div class="row">
+		<div class="col-lg-12">
+			<div class="bs-component">
+				<ul class="nav nav-tabs">
+					<li class="active">
+						<a href="#cancel" data-toggle="tab">
+						<i class="fa fa-sign-out" style="font-size: 64px; font-weight: lighter; padding: 20px 0px 5px;"></i>
+						<br>
+						Cancel subscription anytime
+						</a>
+					</li>
+					<li>
+						<a href="#anywhere" data-toggle="tab">
+						<i class="fa fa-laptop" style="font-size: 64px; font-weight: lighter; padding: 20px 0px 5px;"></i>
+						<br>
+						Watch from anywhere
+						</a>
+					</li>
+					<li>
+						<a href="#price" data-toggle="tab">
+						<i class="fa fa-tags fa-flip-horizontal" style="font-size: 64px; font-weight: lighter; padding: 20px 0px 5px;"></i>
+						<br>
+						Pricing packages
+						</a>
+					</li>
+				</ul>
+				<div id="myTabContent" class="tab-content">
+					<div class="tab-pane active in" id="cancel">
+						<p>
+						<div class="row">
+							<div class="col-md-7" style="padding-top: 50px;">
+								<h4>
+									If you decide DormStartups.tv isn't for you - no problem. <br>No commitment. Cancel online anytime.
+								</h4>
+								<br>
+								<a href="<?php echo base_url();?>index.php?home/signup" class="btn btn-danger btn-lg" >JOIN TODAY</a>
+							</div>
+							<div class="col-md-5">
+								<img src="<?php echo base_url();?>assets/frontend/flixer/images/asset_cancelanytime_withdevice.png" style="width: 100%;" />
+							</div>
+						</div>
+						</p>
+					</div>
+					<div class="tab-pane" id="anywhere">
+						<p>
+						<div class="row">
+							<div class="col-md-9">
+								<h4>
+									Watch TV shows and movies anytime, anywhere. From any device.
+								</h4>
+							</div>
+							<div class="col-md-3">
+								<a href="<?php echo base_url();?>index.php?home/signup" class="btn btn-danger btn-lg" >JOIN TODAY</a>
+							</div>
+						</div>
+						<div class="row" style="margin-top:50px;text-align: center;">
+							<div class="col-md-4">
+								<img src="<?php echo base_url();?>assets/frontend/flixer/images/asset_TV_UI.png" style="width: 100%;" />
+								<h5>Watch on your tv</h5>
+							</div>
+							<div class="col-md-4">
+								<img src="<?php echo base_url();?>assets/frontend/flixer/images/asset_mobile_tablet_UI_2.png" style="width: 100%;" />
+								<h5>Watch on your phone, tablet</h5>
+							</div>
+							<div class="col-md-4">
+								<img src="<?php echo base_url();?>assets/frontend/flixer/images/asset_website_UI.png" style="width: 100%;" />
+								<h5>Watch on your pc</h5>
+							</div>
+						</div>
+						</p>
+					</div>
+					<div class="tab-pane" id="price">
+						<p>
+						<div class="row" style="margin: 35px;">
+							<div class="col-md-8" style="text-align: right;">
+								<h4>
+									Choose one plan and watch everything.
+								</h4>
+							</div>
+							<div class="col-md-4" style="text-align: left;">
+								<a href="<?php echo base_url();?>index.php?home/signup" class="btn btn-danger btn-lg" >JOIN TODAY</a>
+							</div>
+						</div>
+						<!-- price table -->
+						<table class="table table-striped table-hover" style="color: #999;">
+							<tbody>
+								<tr>
+									<td></td>
+									<?php
+										$plans = $this->crud_model->get_active_plans();
+										foreach ($plans as $row):
+										?>
+									<td align="center">
+										<h5 style="text-transform: uppercase;"><?php echo $row['name'];?></h5>
+									</td>
+									<?php endforeach;?>
+								</tr>
+								<tr>
+									<td>Monthly price</td>
+									<?php
+										$plans = $this->crud_model->get_active_plans();
+										foreach ($plans as $row):
+										?>
+									<td align="center">USD <?php echo $row['price'];?></td>
+									<?php endforeach;?>
+								</tr>
+								<tr>
+									<td>Screens you can watch on at the same time</td>
+									<?php
+										$plans = $this->crud_model->get_active_plans();
+										foreach ($plans as $row):
+										?>
+									<td align="center"><?php echo $row['screens'];?></td>
+									<?php endforeach;?>
+								</tr>
+								<tr>
+									<td>Watch on your laptop, TV, phone and tablet</td>
+									<?php
+										$plans = $this->crud_model->get_active_plans();
+										foreach ($plans as $row):
+										?>
+									<td align="center"><i class="fa fa-check" aria-hidden="true"></i></td>
+									<?php endforeach;?>
+								</tr>
+								<tr>
+									<td>HD available</td>
+									<?php
+										$plans = $this->crud_model->get_active_plans();
+										foreach ($plans as $row):
+										?>
+									<td align="center"><i class="fa fa-check" aria-hidden="true"></i></td>
+									<?php endforeach;?>
+								</tr>
+								<tr>
+									<td>Unlimited movies and TV shows</td>
+									<?php
+										$plans = $this->crud_model->get_active_plans();
+										foreach ($plans as $row):
+										?>
+									<td align="center"><i class="fa fa-check" aria-hidden="true"></i></td>
+									<?php endforeach;?>
+								</tr>
+								<tr>
+									<td>Cancel anytime</td>
+									<?php
+										$plans = $this->crud_model->get_active_plans();
+										foreach ($plans as $row):
+										?>
+									<td align="center"><i class="fa fa-check" aria-hidden="true"></i></td>
+									<?php endforeach;?>
+								</tr>
+							</tbody>
+						</table>
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<?php include 'footer.php';?>
+</div>
